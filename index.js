c5e0df09c2234a2a1bf5335b6fdd008d1482e003
”Vladislav <monsterd161@gmail.com>
2021-05-29 19:59:51 +0300

    mod

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 let http = require("http");
 const MongoClient = require("mongodb").MongoClient;
 
-http.createServer(function (request, response) {}).listen(3000);
+http
+  .createServer(function (request, response) {
+    const mongoClient = new MongoClient("mongodb://localhost:27017/", {
+      useUnifiedTopology: true,
+    });
 
-const mongoClient = new MongoClient("mongodb://localhost:27017/", {
-  useUnifiedTopology: true,
-});
+    let bool = { bool: false };
 
-let bool = { bool: false };
-
-mongoClient.connect(function (err, client) {
-  const db = client.db("test");
-  const col = db.collection("test");
-  col.insertOne(bool, function (err, result) {
-    col.findOneAndUpdate(
-      { bool: false },
-      { $set: { bool: true } },
-      {
-        returnOriginal: false,
-      },
-      function (err, result) {
-        console.log(result);
-        client.close();
-      }
-    );
-  });
-});
+    mongoClient.connect(function (err, client) {
+      const db = client.db("test");
+      const col = db.collection("test");
+      col.insertOne(bool, function (err, result) {
+        col.findOneAndUpdate(
+          { bool: false },
+          { $set: { bool: true } },
+          {
+            returnOriginal: false,
+          },
+          function (err, result) {
+            console.log(result);
+            client.close();
+          }
+        );
+      });
+    });
+  })
+  .listen(3000);
